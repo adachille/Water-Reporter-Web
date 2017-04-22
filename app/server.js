ac10b1b8db43e473ef482eb4f900c27d99ff4a47
adachille <adachille15@gmail.com>
2017-04-22 10:21:54 -0400

    Added project structure and dashboard page

---
+++ app/server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+
+// Uses public folder for static files
+app.use(express.static('public'));
+
+/*
+Responses for different requests from pages
+*/
+// Homepage requests
+app.get('/', function(req, res) {
+    console.log("Got a GET request for the homepage, "
+        + "sending welcome-screen");
+    res.sendFile( __dirname + "/public/templates/welcome-screen.html");
+});
+
+// Log-in page requests
+app.get('/login', function(req, res) {
+    console.log("Got a GET request for the login page, "
+        + "sending log-in");
+    res.sendFile( __dirname + "/public/templates/log-in.html");
+});
+app.get('/log-in', function(req, res) {
+    console.log("Got a GET request for the login page, "
+        + "sending log-in");
+    res.sendFile( __dirname + "/public/templates/log-in.html");
+});
+
+// Register page requests
+app.get('/register', function(req, res) {
+    console.log("Got a GET request for the register page, "
+        + "sending register");
+    res.sendFile( __dirname + "/public/templates/register.html");
+});
+
+// Dashboard page requests
+app.get('/dashboard', function(req, res) {
+    console.log("Got a GET request for the dashboard page, "
+        + "sending dashboard");
+    res.sendFile( __dirname + "/public/templates/dashboard.html");
+});
+
+
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Water Reporter listening at http://127.0.0.1:8081")
+})
