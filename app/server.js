d795a9c5cd185b35b8bbb5a81effc0ec1b04e511
adachille <adachille15@gmail.com>
2017-04-25 13:51:21 -0400

    Sign-in and Log-in Functional

--- app/server.js
+++ app/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var google_auth = require('google-auth-library');
 var firebase = require("firebase");
 // Initialize Firebase
 var config = {
-  apiKey: " AIzaSyAFL8ySS_K0tQWW6KxRp5TqLNAYJuflsVw",
+  apiKey: "AIzaSyBMqKgR6hjywnkKPNw9kgPNdLAV0_3bA2Q",
   authDomain: "cs2340-cb649.firebaseapp.com",
   databaseURL: "https://cs2340-cb649.firebaseio.com/",
 };
@@ -13,6 +14,10 @@ var database = firebaseApp.database();
 
 // Uses public folder for static files
 app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 /*
 Responses for different requests from pages
@@ -35,6 +40,20 @@ app.get('/log-in', function(req, res) {
         + "sending log-in");
     res.sendFile( __dirname + "/public/templates/log-in.html");
 });
+app.post('/login', function(req, res) {
+    console.log("Got a POST request from login page to "
+    + "create sign in user.");
+    var email = req.body.email;
+    var password = req.body.password;
+
+
+    firebase.auth().signInWithEmailAndPassword(email,
+        password).catch(function(error) {
+            console.log(error.message);
+            res.end();
+    });
+    res.sendFile( __dirname + "/public/templates/dashboard_home.html");
+});
 
 // Register page requests
 app.get('/register', function(req, res) {
@@ -42,12 +61,30 @@ app.get('/register', function(req, res) {
         + "sending register");
     res.sendFile( __dirname + "/public/templates/register.html");
 });
+app.post('/register', function(req, res) {
+    console.log("Got a POST request from register page to "
+        + "create new user.")
+    var email = req.body.email;
+    var password = req.body.password;
+    firebase.auth().createUserWithEmailAndPassword(email,
+        password).catch(function(error) {
+            console.log(error.message);
+            res.end();
+        });
+    res.sendFile( __dirname + "/public/templates/profile-info.html");
+});
 
 // User Dashboard page requests
 app.get('/dashboard', function(req, res) {
     console.log("Got a GET request for the dashboard page, "
         + "sending dashboard");
-    res.sendFile( __dirname + "/public/templates/dashboard_home.html");
+    var user = firebase.auth().currentUser;
+    if (user) {
+        res.sendFile( __dirname + "/public/templates/dashboard_home.html");
+    } else {
+        console.log("User not signed in, cannot access dashboard.");
+        res.status(403);
+    }
 });
 
 //Worker Dashboard page requests
@@ -77,6 +114,15 @@ app.get('/profile', function(req, res) {
     console.log("Got a GET request for the profile, "
         + "sending profile");
     res.sendFile( __dirname + "/public/templates/profile-info.html");
+});
+app.post('/profile', function(req, res) {
+    var user = firebase.auth().currentUser;
+    if (user) {
+        res.sendFile( __dirname + "/public/templates/profile-info.html");
+    } else {
+        console.log("User not signed in, cannot access dashboard.");
+        res.status(403);
+    }
 });
 
 
