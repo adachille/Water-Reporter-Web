042e21687a10d67ded5eeb35d4cd9673844eaae7
adachille <adachille15@gmail.com>
2017-04-25 14:39:16 -0400

    Worked further on signing in/logging out

--- app/server.js
+++ app/server.js
@@ -19,15 +19,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+
 /*
 Responses for different requests from pages
 */
+
 // Homepage requests
 app.get('/', function(req, res) {
     console.log("Got a GET request for the homepage, "
         + "sending welcome-screen");
     res.sendFile( __dirname + "/public/templates/welcome-screen.html");
 });
+
 
 // Log-in page requests
 app.get('/login', function(req, res) {
@@ -40,13 +43,14 @@ app.get('/log-in', function(req, res) {
         + "sending log-in");
     res.sendFile( __dirname + "/public/templates/log-in.html");
 });
+
 app.post('/login', function(req, res) {
     console.log("Got a POST request from login page to "
     + "create sign in user.");
     var email = req.body.email;
     var password = req.body.password;
 
-
+    // Logs user in
     firebase.auth().signInWithEmailAndPassword(email,
         password).catch(function(error) {
             console.log(error.message);
@@ -55,13 +59,16 @@ app.post('/login', function(req, res) {
     res.sendFile( __dirname + "/public/templates/dashboard_home.html");
 });
 
+
 // Register page requests
 app.get('/register', function(req, res) {
     console.log("Got a GET request for the register page, "
         + "sending register");
     res.sendFile( __dirname + "/public/templates/register.html");
 });
+
 app.post('/register', function(req, res) {
+    // Create new user
     console.log("Got a POST request from register page to "
         + "create new user.")
     var email = req.body.email;
@@ -69,10 +76,38 @@ app.post('/register', function(req, res) {
     firebase.auth().createUserWithEmailAndPassword(email,
         password).catch(function(error) {
             console.log(error.message);
-            res.end();
-        });
+    });
+
     res.sendFile( __dirname + "/public/templates/profile-info.html");
 });
+
+
+// Profile page requests
+app.get('/profile', function(req, res) {
+    console.log("Got a GET request for the profile, "
+        + "sending profile");
+
+    // Opens profile page if logged on, else send to login page
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            res.sendFile( __dirname + "/public/templates/profile-info.html");
+        } else {
+            console.log("User is not logged in");
+            res.sendFile( __dirname + "/public/templates/log-in.html");
+        }
+    });
+});
+
+app.post('/profile', function(req, res) {
+    var user = firebase.auth().currentUser;
+    if (user) {
+        res.sendFile( __dirname + "/public/templates/profile-info.html");
+    } else {
+        console.log("User not signed in, cannot access dashboard.");
+        res.status(403);
+    }
+});
+
 
 // User Dashboard page requests
 app.get('/dashboard', function(req, res) {
@@ -87,6 +122,7 @@ app.get('/dashboard', function(req, res) {
     }
 });
 
+
 //Worker Dashboard page requests
 app.get('/workerdashboard', function(req, res) {
     console.log("Got a GET request for the worker dashboard page, "
@@ -94,12 +130,14 @@ app.get('/workerdashboard', function(req, res) {
     res.sendFile( __dirname + "/public/templates/worker_dashboard_home.html");
 });
 
+
 //Water Source Report  page requests
 app.get('/submitSourceReport', function(req, res) {
     console.log("Got a GET request for the submit water source report page, "
         + "submit water source report");
     res.sendFile( __dirname + "/public/templates/sourceReport.html");
 });
+
 
 //Water Purity Report page requests
 app.get('/submitPurityReport', function(req, res) {
@@ -109,26 +147,10 @@ app.get('/submitPurityReport', function(req, res) {
 });
 
 
-// Profile page requests
-app.get('/profile', function(req, res) {
-    console.log("Got a GET request for the profile, "
-        + "sending profile");
-    res.sendFile( __dirname + "/public/templates/profile-info.html");
-});
-app.post('/profile', function(req, res) {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        res.sendFile( __dirname + "/public/templates/profile-info.html");
-    } else {
-        console.log("User not signed in, cannot access dashboard.");
-        res.status(403);
-    }
-});
-
 
 var server = app.listen(8081, function() {
     var host = server.address().address
     var port = server.address().port
 
     console.log("Water Reporter listening at http://127.0.0.1:8081")
-})
+});
